028cd3889c3a96cb92e3482b4d97c13937fc9fba
Leandro César <ccleandroc@gmail.com>
2023-02-08 13:17:12 -0300

    refactor: remove `create_app` method

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, Response, redirect, render_template
+from flask import Flask, Response, render_template
 from flask_caching import Cache
 from flask_cors import CORS
 from flask_graphql import GraphQLView
@@ -13,9 +13,14 @@ from app.graphql import schema
 app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app)
 app.url_map.strict_slashes = False
-cache = Cache()
-cors = CORS()
-limiter = Limiter(get_remote_address, storage_uri="memory://")
+cache = Cache(app)
+cors = CORS(app)
+limiter = Limiter(get_remote_address, app=app, storage_uri="memory://")
+app.add_url_rule(
+    "/graphql",
+    view_func=GraphQLView.as_view("graphql", schema=schema, graphiql=True),
+)
+api.init_app(app)
 
 
 @app.route("/ping")
@@ -35,18 +40,5 @@ def create_data() -> None:
     data.from_file("app/data.json")
 
 
-def create_app() -> Flask:
-    cors.init_app(app)
-    cache.init_app(app)
-    limiter.init_app(app)
-    api.init_app(app)
-    app.add_url_rule(
-        "/graphql",
-        view_func=GraphQLView.as_view("graphql", schema=schema, graphiql=True),
-    )
-    return app
-
-
 if __name__ == "__main__":
-    app = create_app()
     app.run()
