e40155e5140e3b3aa16c5b20f3a804fb08c4330a
Leandro César <ccleandroc@gmail.com>
2023-02-08 23:58:45 -0300

    feat: add scraper code

--- scripts/scraper.py
+++ scripts/scraper.py
@@ -1,191 +1,112 @@
 import json
 import re
-from datetime import datetime
+from enum import Enum
 
-import requests
-from bs4 import BeautifulSoup, ResultSet
-from tqdm import tqdm
-from unidecode import unidecode
-
-NUM_EDITIONS = 95
-# from: https://en.wikipedia.org/wiki/Academy_Awards#Awards_of_Merit_categories
-CATEGORIES = [
-    "Best Animated Feature Film",
-    "Best Makeup and Hairstyling",
-    "Best Costume Design",
-    "Best International Feature Film",
-    "Best Documentary Feature Film",
-    "Best Documentary Short Film",
-    "Best Original Screenplay",
-    "Best Visual Effects",
-    "Best Supporting Actor",
-    "Best Supporting Actress",
-    "Best Film Editing",
-    "Best Original Score",
-    "Best Original Song",
-    "Best Live Action Short Film",
-    "Best Animated Short Film",
-    "Best Sound",
-    "Best Picture",
-    "Best Director",
-    "Best Actor",
-    "Best Actress",
-    "Best Cinematography",
-    "Best Production Design",
-    "Best Adapted Screenplay",
-    "Best Assistant Director",
-    "Best Director, Comedy Picture",
-    "Best Director, Dramatic Picture",
-    "Best Dance Direction",
-    "Best Original Musical or Comedy Score",
-    "Best Original Story",
-    "Best Short Subject, 1 Reel",
-    "Best Short Subject, 2 Reel",
-    "Best Short Subject, Color",
-    "Best Short Subject, Comedy",
-    "Best Short Subject, Novelty",
-    "Best Sound Editing",
-    "Best Title Writing",
-    "Best Unique and Artistic Production",
-]
-REGEX_DATE = (
-    r"(?:January|February|March|April|May|June|July|August|September|October|November|December)"
-    r"\s[0-9]{1,2},"
-    r"\s(?:19|20)[0-9]{2}"
+REGEX_YEAR_EDITION = re.compile(
+    r"^((?:19|20)[0-9]{2})(?:\/[0-9]{2})? \(([0-9]{1,2})(?:st|nd|rd|th)\)$"
 )
+REGEX_NOTE = re.compile(r"^\[NOTE\: (.+)\]$")
+REGEX_CATEGORY = re.compile(r"^[A-Z ?]{4,}(?:\([A-Za-z- ]+\))?$")
 
 
 def to_ordinal(number: int) -> str:
-    if str(number)[-1] == "1" and str(number) != "11":
+    if 11 <= number <= 13:
+        return f"{number}th"
+    elif str(number)[-1] == "1":
         return f"{number}st"
-    if str(number)[-1] == "2" and str(number) != "12":
+    elif str(number)[-1] == "2":
         return f"{number}nd"
-    if str(number)[-1] == "3" and str(number) != "13":
+    elif str(number)[-1] == "3":
         return f"{number}rd"
     return f"{number}th"
 
 
-def clean_text(text: str) -> str:
-    text = unidecode(text).split("\n")[0]
-    text = re.sub(r"\[[0-9a-zA-Z]{1,2}\]", "", text)
-    text = text.strip().rstrip("*")
-    return text
-
-
-def get_date(value: str) -> str:
-    result = re.search(REGEX_DATE, value, re.IGNORECASE)
-    if not result:
-        raise ValueError(f"No date was found in {value!r}")
-    return datetime.strptime(result.group(0), "%B %d, %Y").date().isoformat()
-
-
-def get_categories_from_tr(
-    table: ResultSet,
-) -> list[dict[str, str | list[dict[str, str]]]]:
-    trs = table.tbody.find_all("tr")
-    data = []
-    for tr in trs:
-        ths = tr.find_all("th")
-        tds = tr.find_all("td")
-        for th in ths:
-            if th.get_text() and th.get_text() != "\n":
-                item = {"name": clean_text(th.get_text())}
-                data.append(item)
-        for td in tds:
-            if hasattr(td, "ul") and td.ul:
-                nominees = td.ul.get_text().split("\n")
-                item = {"nominees": list(map(clean_text, nominees))}
-                data.append(item)
-    part_one = data[::2]
-    part_two = data[1::2]
-    if (len(data) / 2 % 2) != 0:
-        part_two = part_two[-1:] + part_two[:-1]
-    data = part_one + part_two
-    data = [{**data[i], **data[i + 1]} for i in range(0, len(data), 2)]
+def read_file(filename: str) -> list[str]:
+    with open(filename) as f:
+        data = f.readlines()
     return data
 
 
-def get_categories_from_td(
-    table: ResultSet,
-) -> list[dict[str, str | list[dict[str, str]]]]:
-    tds = table.tbody.find_all("td")
-    data = []
-    for td in tds:
-        item = {}
-        if hasattr(td, "div") and td.div:
-            item["name"] = clean_text(td.div.get_text())
-        if hasattr(td, "ul") and td.ul:
-            nominees = td.ul.get_text().split("\n")
-            item["nominees"] = list(map(clean_text, nominees))
-        if item:
-            data.append(item)
-    return data
+def write_file(filename: str, data: list[str]) -> None:
+    with open(filename, "w") as f:
+        f.write(json.dumps(data))
 
 
-def get_categories_from_table(
-    table: ResultSet,
-) -> list[dict[str, str | list[dict[str, str]]]]:
-    trs = table.tbody.find_all("tr")
-    if any(tr.th and tr.th.get_text() != "\n" for tr in trs):
-        categories = get_categories_from_tr(table)
-    else:
-        categories = get_categories_from_td(table)
-    for category in categories:
-        nominees = []
-        for nominee in category["nominees"]:
-            if " - " in nominee:
-                name, extra = nominee.split(" - ", 1)
-            else:
-                name, extra = nominee, None
-            nominee = {"name": name, "extra": extra}
-            nominees.append(nominee)
-        category["nominees"] = nominees
-        category["winner"] = category["nominees"][0]
-    return categories
+def find_category(value: str) -> re.Match | None:
+    return REGEX_CATEGORY.match(value)
 
 
-def get_date_from_table(table: ResultSet) -> str | None:
-    for tr in table.find_all("tr"):
-        if tr.th and tr.td and tr.th.get_text().lower() == "date":
-            value = tr.td.get_text()
-            return get_date(value)
+def find_note(value: str) -> re.Match | None:
+    return REGEX_NOTE.match(value)
 
 
-def scraper(
-    content: str,
-) -> dict[str, str | list[dict[str, str | list[dict[str, str]]]]]:
-    soup = BeautifulSoup(content, "html.parser")
-    title = soup.h1.get_text()
+def find_year_and_edition(value: str) -> re.Match | None:
+    return REGEX_YEAR_EDITION.match(value)
 
-    for table in soup.find_all("table"):
-        if "infobox" in table["class"]:
-            date = get_date_from_table(table)
-        if "wikitable" in table["class"]:
-            categories = get_categories_from_table(table)
-            break
 
+def new_edition(year_edition: re.Match) -> dict[str, str]:
     return {
-        "name": title,
-        "date": date,
-        "categories": categories,
+        "name": f"{to_ordinal(int(year_edition.group(2)))} Academy Awards",
+        "year": year_edition.group(1),
+        "edition": int(year_edition.group(2)),
+        "categories": [],
     }
 
 
+def new_category(category: re.Match) -> dict[str, str]:
+    return {
+        "name": category.group(0).title(),
+        "nominees": [],
+    }
+
+
+def new_nominee(value: str) -> dict[str, str]:
+    if " -- " in value:
+        name, more = value.split(" -- ", maxsplit=1)
+    else:
+        name, more = value, None
+    return {"name": name, "more": more, "note": None, "winner": False}
+
+
+def raw_data_to_json(raw_data: list[str]) -> list[dict]:
+    data: list[dict] = []
+    edition = category = nominee = {}
+    for line in raw_data:
+        line = line.strip(" \n")
+        _year_edition = find_year_and_edition(line)
+        if _year_edition:
+            edition = new_edition(_year_edition)
+            data.append(edition)
+            continue
+        _category = find_category(line)
+        if _category:
+            category = new_category(_category)
+            edition["categories"].append(category)
+            continue
+        _note = find_note(line)
+        if _note:
+            nominee["note"] = _note.group(1)
+            continue
+        nominee = new_nominee(line)
+        category["nominees"].append(nominee)
+    return data
+
+
+def merge_jsons(*, base: list[dict], append: list[dict]) -> list[dict]:
+    data = base.copy()
+    for i, edition in enumerate(append):
+        for j, category in enumerate(edition["categories"]):
+            nominee = category["nominees"][0]
+            for n in data[i]["categories"][j]["nominees"]:
+                if nominee["name"] == n["name"]:
+                    n["winner"] = True
+    return data
+
+
 if __name__ == "__main__":
-
-    with open("app/data.json") as f:
-        raw_data = f.read() or "[]"
-        datas = json.loads(raw_data)
-
-    init = len(datas) + 1
-
-    for i in tqdm(range(init, NUM_EDITIONS), initial=init):
-        edition = to_ordinal(i)
-        url = f"https://en.wikipedia.org/wiki/{edition}_Academy_Awards"
-        response = requests.get(url, timeout=5)
-        response.raise_for_status()
-        data = scraper(response.content)
-        datas.append(data)
-
-        with open("app/data.json", "w") as f:
-            f.write(json.dumps(datas))
+    raw_data = read_file("scripts/raw_data_nominees.txt")
+    nominees = raw_data_to_json(raw_data)
+    raw_data = read_file("scripts/raw_data_winners.txt")
+    winners = raw_data_to_json(raw_data)
+    data = merge_jsons(base=nominees, append=winners)
+    write_file("app/data.json", data)
